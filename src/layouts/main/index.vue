7af756ea39a70a1ec5de76d55bc92434c91a79cf
吴也凡 <2449185223@qq.com>
2022-10-13 17:43:15 +0800

    docs(初始化架构): 初始化架构

---
+++ src/layouts/main/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-container class="common-layout">
+      <el-container>
+        <el-aside>
+        </el-aside>
+        <el-container>
+          <el-header>
+            <HeaderComponent />
+          </el-header>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeaderComponent from "./header.vue";
+import AsideComponent from "./aside.vue";
+const props = defineProps({
+  menus: [],
+});
+</script>
+
+<style lang="less" scoped></style>
